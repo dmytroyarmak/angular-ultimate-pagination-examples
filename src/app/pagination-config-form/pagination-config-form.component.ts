0ceb9855cc20118bb9f21ca86a6224c864ee93bb
Dmytro Yarmak <dmytroy@backbase.com>
2017-05-27 20:27:37 +0300

    Change component's prefix to 'ultimate'

--- src/app/pagination-config-form/pagination-config-form.component.ts
+++ src/app/pagination-config-form/pagination-config-form.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
 
 @Component({
-  selector: 'app-pagination-config-form',
+  selector: 'ultimate-pagination-config-form',
   templateUrl: './pagination-config-form.component.html',
   styleUrls: ['./pagination-config-form.component.css']
 })
